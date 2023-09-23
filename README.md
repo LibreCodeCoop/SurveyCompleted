338250db7965a8beacc741860e530f8d272b2a2c
Vitor Mattos <vitor@php.rio>
2023-09-22 21:49:23 -0300

    First commit
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ README.md
@@ -0,0 +1,19 @@
+# LimeSurvey SurveyCompleted Plugin
+
+The Limesurvey SurveyCompleted plugin allow you to define an url to redirect the user when the survey is completed.
+
+## Plugin Installation
+
+- Copy the SurveyCompleted folder to the Limesurvey "plugins" directory.
+- Go to `SurveyCompleted` folder
+- Activate the plugin at the Limesurvey plugin manager (requires proper user rights for accessing the feature at the Limesurvey admin interface).
+- Configure the plugin at the settings page
+
+<img src="img/settings.png" />
+
+### Custom settings by survey
+You can add custom settings by survey.
+
+- Go to survey settings
+- GO to `Simple plugins`
+- Define your custom settings at `Settings for plugin SurveyCompleted `
---
+++ SurveyCompleted.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Class SurveyCompleted
+ */
+class SurveyCompleted extends PluginBase
+{
+    /**
+     * @var string
+     */
+    static protected $description = 'Survey completed plugin';
+
+    /**
+     * @var string
+     */
+    static protected $name = 'SurveyCompleted';
+
+    protected $settings = [
+        'RedirectUrl' => [
+            'type' => 'string',
+            'label' => 'Redirect url',
+            'help' => 'URL to redirect the form when already filled.',
+        ],
+    ];
+
+    /**
+     * @return void
+     */
+    public function init()
+    {
+        $this->subscribe('onSurveyDenied');
+        $this->subscribe('newSurveySettings');
+        $this->subscribe('beforeSurveySettings');
+    }
+
+    /**
+     * @return void
+     */
+    public function onSurveyDenied()
+    {
+        $event = $this->getEvent();
+        $reason = $event->get('reason');
+        if ($reason !== 'alreadyCompleted') {
+            return;
+        }
+        $surveyId = $event->get('surveyId');
+        $redirectUrl = $this->get(
+            'RedirectUrl',
+            'Survey',
+            $surveyId, // Survey
+            $this->get('RedirectUrl') // Global
+        );
+        if (!$redirectUrl) {
+            return;
+        }
+        header('Location: ' . $redirectUrl);
+    }
+
+    /**
+     * @return void
+     */
+    public function newSurveySettings()
+    {
+        $event = $this->getEvent();
+
+        foreach ($event->get('settings') as $name => $value) {
+            $this->set($name, $value, 'Survey', $event->get('survey'));
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function beforeSurveySettings()
+    {
+        $event = $this->getEvent();
+        $event->set(
+            "surveysettings.{$this->id}",
+            [
+                'name' => get_class($this),
+                'settings' => [
+                    'SettingsInfo' => [
+                        'type' => 'info',
+                        'content' => '<legend><small>Redirect URL settings</small></legend>'
+                    ],
+                    'RedirectUrl' => [
+                        'type' => 'string',
+                        'label' => $this->settings['RedirectUrl']['help'],
+                        'help' => $this->settings['RedirectUrl']['help'],
+                        'current' => $this->get(
+                            'RedirectUrl',
+                            'Survey',
+                            $event->get('survey'), // Survey
+                            $this->get('RedirectUrl') // Global
+                        ),
+                    ],
+                ]
+            ]
+        );
+    }
+}
